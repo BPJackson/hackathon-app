3d5bb84552fa36733259c57f835f50871c4c446f
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-07-07 10:58:33 -0600

    added app

--- router.js
+++ router.js
@@ -17,9 +17,6 @@ routes.addRoute('/home', (req, res, url) => {
     })
 routes.addRoute('/counter', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
-    if (req.method === 'POST') {
-      if (req.value)
-
       if (req.method === 'POST') {
           var data = ''
           req.on('data', function (chunk) {
@@ -27,32 +24,37 @@ routes.addRoute('/counter', (req, res, url) => {
           })
           req.on('end', function () {
             var counter = qs.parse(data)
-            if (counter.value === clicked) counter.value = true
-            var insertMotivate = bands.insert(band)
-            insertBand.on('success', function() {
-              res.writeHead(302, {'Location': '/bands'})
-              res.end()
-            })
-          })
-        }
-      })
-    
-      var obj = motivate.findOne({isMotivated})
+            if (counter.value === 'true') {
+              var obj = motivate.findOne({isMotivated})
+                obj.isMotivated += 1
+                motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
+                  if (err) res.end('404')
+                  res.writeHead(302, {'Location': '/data'})
+                  res.end()
+})}})})
+            //var insertMotivate = bands.insert(band)
+            //insertBand.on('success', function() {
+              //res.writeHead(302, {'Location': '/bands'})
+              //res.end()
+            //})
+          //})
+        //}
+      //})
+  var obj = motivate.findOne({isMotivated})
+    obj.isMotivated += 1
+    motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
+      if (err) res.end('404')
+      res.writeHead(302, {'Location': '/data'})
+      res.end()
+    })
+    if (req.method === 'POST') {
+      var obj = motivate.findOne({notMotivated})
       obj.isMotivated += 1
       motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
         if (err) res.end('404')
         res.writeHead(302, {'Location': '/data'})
         res.end()
       })
-      if (req.method === 'POST') {
-        var obj = motivate.findOne({notMotivated})
-        obj.isMotivated += 1
-        motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
-          if (err) res.end('404')
-          res.writeHead(302, {'Location': '/data'})
-          res.end()
-        })
-
 
 routes.addRoute('/data', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
