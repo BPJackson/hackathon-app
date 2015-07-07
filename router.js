a3dd0fa11b9c5fbaca21af061ea58d0e887676b6
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-07-07 11:49:40 -0600

    Works

--- router.js
+++ router.js
@@ -1,41 +1,65 @@
 var routes = require('routes')(),
-    fs = require('fs'),
-    db = require('monk')('localhost/motivate'),
-    bands = db.get('bands'),
-    qs = require('qs'),
-    // view = require('./view'),
-    mime = require('mime')
+  fs = require('fs'),
+  db = require('monk')('localhost/motivate'),
+  counter = db.get('counter'),
+  qs = require('qs'),
+  view = require('./view'),
+  mime = require('mime')
 
 // install fs monk qs mustache mime
 routes.addRoute('/home', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-        var file = fs.readFile('templates/home.html' function(err, file) {
-          if (err) res.end('404')
-        })
-        res.end(file)
+    var file = fs.readFile('templates/home.html', function (err, file) {
+      if (err) res.end('404')
+      res.end(file)
+    })
+  }
+})
+routes.addRoute('/counter', (req, res, url) => {
+  res.setHeader('Content-Type', 'text/html')
+  if (req.method === 'POST') {
+    console.log(req.method)
+    var data = ''
+    req.on('data', function (chunk) {
+      data += chunk
+    })
+    req.on('end', function () {
+      var counters = qs.parse(data)
+      console.log(counters)
+      if (counters.isMotivated === 'true') {
+        counter.update({}, {$inc: {isMotivated: 1}})
+      }
+      else {
+        counter.update({}, {$inc: {notMotivated: 1}})
       }
     })
-// routes.addRoute('/counter', (req, res, url) => {
-//   res.setHeader('Content-Type', 'text/html')
-//       if (req.method === 'POST') {
-//           var data = ''
-//           req.on('data', function (chunk) {
-//             data += chunk
-//           })
-//           req.on('end', function () {
-//             var counter = qs.parse(data)
-//             if (counter.value === 'true') {
-//               var obj = motivate.findOne({isMotivated})
-//                 obj.isMotivated += 1
-//                 motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
-//                   if (err) res.end('404')
-//                   res.writeHead(302, {'Location': '/data'})
-//                   res.end()
-//                 })
-//               }})
-//             }
-//           })
+  }
+})
+//             //var insertMotivate = bands.insert(band)
+//             //insertBand.on('success', function() {
+//               //res.writeHead(302, {'Location': '/bands'})
+//               //res.end()
+//             //})
+//           //})
+//         //}
+//       //})
+//   var obj = motivate.findOne({isMotivated})
+//     obj.isMotivated += 1
+//     motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
+//       if (err) res.end('404')
+//       res.writeHead(302, {'Location': '/data'})
+//       res.end()
+//     })
+//     if (req.method === 'POST') {
+//       var obj = motivate.findOne({notMotivated})
+//       obj.isMotivated += 1
+//       motivate.update({_id: url.params.id}, {$set: obj}, function (err, doc) {
+//         if (err) res.end('404')
+//         res.writeHead(302, {'Location': '/data'})
+//         res.end()
+//       })
+
 // routes.addRoute('/data', (req, res, url) => {
 //   res.setHeader('Content-Type', 'text/html')
 //   if (req.method === 'GET') {
@@ -57,14 +81,16 @@ routes.addRoute('/home', (req, res, url) => {
 //       })
 //     })
 //   }
+// })
 routes.addRoute('/public/*', (req, res, url) => {
   res.setHeader('Content-Type', mime.lookup(req.url))
   fs.readFile('./' + req.url, function (err, file) {
-   if (err) {
-     res.setHeader('Content-Type', 'text/html')
-     res.end('404')
-     }
-     res.end(file)
+    if (err) {
+      res.setHeader('Content-Type', 'text/html')
+      res.end('404')
+    }
+    res.end(file)
   })
 })
+
 module.exports = routes
